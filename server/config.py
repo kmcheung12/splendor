45de029dc35ea5f23d44dd09816b6ffb2c1394ab
Alan <kmcheung12@gmail.com>
2026-06-02 22:46:15 +0100

    feat: add GameConfig dataclass

---
+++ server/config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class SlotConfig:
+    index: int
+    agent_type: str  # "random", "mcts", "early-capture", etc.
+
+
+@dataclass
+class GameConfig:
+    num_players: int
+    slots: list[SlotConfig]
+    delay_ms: int = 800
+
+    def __post_init__(self):
+        assert 2 <= self.num_players <= 4
+        assert len(self.slots) == self.num_players
+        assert self.delay_ms >= 0
